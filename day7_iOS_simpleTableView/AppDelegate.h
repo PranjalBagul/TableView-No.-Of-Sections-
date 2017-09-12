95981127bae6313e831b9e5792e299b03cfec71f
Student P_08 <studentp_08@Student-P-08.local>
2017-09-12 11:16:31 +0530

    Initial Commit

---
+++ day7_iOS_simpleTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  day7_iOS_simpleTableView
+//
+//  Created by Student P_08 on 12/09/17.
+//  Copyright © 2017 pranjal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ day7_iOS_simpleTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  day7_iOS_simpleTableView
+//
+//  Created by Student P_08 on 12/09/17.
+//  Copyright © 2017 pranjal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
